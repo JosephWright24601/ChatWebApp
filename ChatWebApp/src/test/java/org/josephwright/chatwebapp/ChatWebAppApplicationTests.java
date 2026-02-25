087a3eab9f1475e82b7b2704899d9aa4c1811dd1
JosephWright24601 <acapulcopotato@gmail.com>
2026-02-25 12:31:25 -0600

    setup

---
+++ ChatWebApp/src/test/java/org/josephwright/chatwebapp/ChatWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josephwright.chatwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
